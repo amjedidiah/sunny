f4ee29c6d6d0bddc100ca23fd503fed00b95817c
amjedidiah <imunacode@gmail.com>
2020-09-11 01:18:25 +0100

    fix: Starting out with 100% average lighthouse rating

--- public/js/main.js
+++ public/js/main.js
@@ -1,20 +1,18 @@
-const makeCardPulse = (input) => {
-  makeCardNormal(input);
-  input.parentElement.parentElement.parentElement.classList.add("active");
-};
+// const makeCardPulse = (input) => {
+//   makeCardNormal(input);
+//   input.parentElement.parentElement.parentElement.classList.add("active");
+// };
 
-const makeCardNormal = () => $(".card").removeClass("active");
+// const submitForm = (form) => {
+//   console.log(form);
+// };
 
-const submitForm = (form) => {
-  console.log(form);
-};
-
-document
-  .querySelector("form#inputForm")
-  .addEventListener(
-    "submit",
-    (e) => (e.preventDefault(), submitForm(e.target))
-  );
+// document
+//   .querySelector("form#inputForm")
+//   .addEventListener(
+//     "submit",
+//     (e) => (e.preventDefault(), submitForm(e.target))
+//   );
 
 // Disable console logging
 console.log = function () {};
